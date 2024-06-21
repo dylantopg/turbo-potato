dc65a1251031a6b8a789d56161a955bea7f88b8f
dylantopg <dylanjose619@udenar.edu.co>
2024-06-20 23:18:12 -0500

    intentando implementar los logicos y los relacionales

--- src/apcompiladorrecursivoatributoel/ApCompiladorRecursivoAtributoEL.java
+++ src/apcompiladorrecursivoatributoel/ApCompiladorRecursivoAtributoEL.java
@@ -1,14 +1,12 @@
 package apcompiladorrecursivoatributoel;
 // este aplicativo utiliza el metodo recursivo descendente para reconocer
-// secuencias aritmeticas lógicas y relacionales
 
-import java.util.*;
-import java.io.*;
+// secuencias aritmeticas lógicas y relacionales
 
 public class ApCompiladorRecursivoAtributoEL {
 
     static Lexico lexico = new Lexico();
-    static String cad = "(2^(5-3))¬";
+    static String cad = "(2<5)¬";
     static String cad1 = "0123456789.";
     // variable indice es global y controla el indice del objwro lex1
     static int indice = 0;
@@ -17,8 +15,10 @@ public class ApCompiladorRecursivoAtributoEL {
     static String cadavance = "";
 
     public static void main(String[] args) {
-        /* InputStreamReader isr= new InputStreamReader(System.in);
-        BufferedReader flujoE = new BufferedReader(isr);*/
+        /*
+         * InputStreamReader isr= new InputStreamReader(System.in);
+         * BufferedReader flujoE = new BufferedReader(isr);
+         */
         analisisLexico();
         cad = lex1.cadenaLexico();
         sim = lex1.darElemento(indice).darTipo();
@@ -33,15 +33,17 @@ public class ApCompiladorRecursivoAtributoEL {
 
     }
 
+    // DESDE AQUI HASTA LA MARCA DE COMENTARIO DE ABAJO SE DEBE AJUSTARLE PARA QUE
+    // TRABAJE CORRECTAMENTE LAS LOGICAS Y LAS RELACIONALES
     public static void procS() {
-        // <S> --> <E> 
+        // <S> --> <E>
 
-        boolean res = false;
+        // boolean res = false;
 
         switch (sim) {
             case 'i':
             case '(':
-                NoTerminal s1 = new NoTerminal("s1", 0, 0);
+                NoTerminal s1 = new NoTerminal("s1", 0, 0, false);
                 procE(s1);
                 resultado(s1.getValor());
                 return;
@@ -51,13 +53,167 @@ public class ApCompiladorRecursivoAtributoEL {
         }
     }
 
+    public static void procELO(NoTerminal s1) {
+        // <ELO> -> <EL2> <ELO_L>
+        switch (sim) {
+            case 'i':
+            case '(':
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false); // Crear NoTerminal con valor lógico inicial falso
+                procEL2(s2);
+                procELO_L(s2, s1); // Llamar procELO_L sin pasar i1
+                return;
+
+            default:
+                System.out.println("Secuencia " + cad + " no se acepta");
+                rechace();
+        }
+    }
+
+    public static void procELO_L(NoTerminal s2, NoTerminal s1) {
+        // <ELO_L> -> | <EL2> <ELO_L> | e
+        switch (sim) {
+            case '|':
+                avance();
+                NoTerminal s3 = new NoTerminal("s3", 0, 0, false);
+                procEL2(s3);
+                procELO_L(s3, s1); // Llamar recursivamente a procELO_L
+
+                boolean resultadoOR = (s2.isVlogico() || s3.isVlogico());
+                s1.setVlogico(resultadoOR);
+                return;
+
+            case ')':
+            case '¬':
+                // Se debe dejar s1 sin modificar si no hay OR
+                return;
+
+            default:
+                System.out.println("Secuencia " + cad + " no se acepta");
+                rechace();
+        }
+    }
+
+    public static void procEL2(NoTerminal s1) {
+        // <EL2> --> <ER><EL2_L>
+        switch (sim) {
+            case 'i':
+            case '(':
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false); // Crear NoTerminal con valor lógico inicial falso
+                procER(s2);
+                procEL2_L(s2, s1); // Llamar procELO_L sin pasar i1
+                return;
+
+            default:
+                System.out.println("Secuencia " + cad + " no se acepta");
+                rechace();
+        }
+    }
+
+    public static void procEL2_L(NoTerminal s2, NoTerminal s1) {
+        // <EL2_L> -> &<ER><EL2_L> | e
+        switch (sim) {
+            case '&':
+                avance();
+                NoTerminal s3 = new NoTerminal("s3", 0, 0, false);
+                procER(s3);
+                procEL2_L(s3, s1); // Llamar recursivamente a procELO_L
+
+                boolean resultadoOR = (s2.isVlogico() && s3.isVlogico());
+                s1.setVlogico(resultadoOR);
+                return;
+
+            case ')':
+            case '¬':
+            case '|':
+                // Se debe dejar s1 sin modificar si no hay OR
+                return;
+
+            default:
+                System.out.println("Secuencia " + cad + " no se acepta");
+                rechace();
+        }
+    }
+
+    public static void procER(NoTerminal s1) {
+        // 8. <ER> s1 -><E> s2 <ER_L>
+        switch (sim) {
+            case 'i':
+            case '(':
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false); // Crear NoTerminal con valor lógico inicial falso
+                procE(s2);
+                procER_L(s2, s1); //
+                return;
+
+            default:
+                System.out.println("Secuencia " + cad + " no se acepta");
+                rechace();
+        }
+    }
+
+    public static void procER_L(NoTerminal s2, NoTerminal s1) {
+        // <EL2_L> -> <OR><E> | e
+        switch (sim) {
+            // HAY QUE CORREGIR PARA QUE RECONOZCA LOS OPERADORES RELACIONALES
+            case '<':
+                avance();
+                NoTerminal s3 = new NoTerminal("s3", 0, 0, false);
+                procER(s3);
+                procEL2_L(s3, s1); // Llamar recursivamente a procELO_L
+
+                boolean resultadoOR = (s2.isVlogico() && s3.isVlogico());
+                s1.setVlogico(resultadoOR);
+                return;
+
+            case '>':
+            case '=':
+            case '!':
+                // HASTA EL !(NOT) HAY QUE DARLE MANEJOS COMO RELACIONALES
+                // DE AQUI PARA ABAJO SON LOS CASOS DEL VACIO
+            case ')':
+            case '¬':
+            case '|':
+                // Se debe dejar s1 sin modificar si no hay OR
+                return;
+
+            default:
+                System.out.println("Secuencia " + cad + " no se acepta");
+                rechace();
+        }
+    }
+
+    public static procOR() {
+        //.<OR>    --> < <ME> | > <MA> | = <IG> | ! <DI>  
+                     
+        
+    }
+
+    // SI TANTOS SON DE IGUAL O VACIO NO SE PUEDE HACER LA RECURSIVIDAD PARA QUE
+    // QUEDE UN SOLO PROCESO QUE SE ENCARGUE DE EL IGUAL O VACIO?
+    public static void procME() {
+        // PUEDE SER = O VACIO
+    }
+
+    public static void procMA() {
+        // PUEDE SER = O VACIO
+    }
+
+    public static void procIG() {
+        // PUEDE SER = O VACIO
+    }
+
+    public static void procDI() {
+        // PUEDE SER = O VACIO
+    }
+
+    // DE AQUI PARA ABAJO sigue todo lo que esta perfecto en aritmetica
+
     public static void procE(NoTerminal s1) {
         // <E> --> <T><E_L>
 
         switch (sim) {
             case 'i':
             case '(':
-                NoTerminal s2 = new NoTerminal("s2", 0, 0);
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false);
                 procT(s2);
                 procEL(s2.getValor(), s1);
                 return;
@@ -70,24 +226,24 @@ public class ApCompiladorRecursivoAtributoEL {
 
     public static void procEL(double i1, NoTerminal s1) {
         // <E_L> --> +<T><E_L>
-        // <E_L> --> -<T><E_L>  no está implementada
+        // <E_L> --> -<T><E_L> no está implementada
         // <E_L> --> e
 
         switch (sim) {
             case '+':
                 avance();
-                NoTerminal s2 = new NoTerminal("s2", 0, 0);
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false);
                 procT(s2);
-                NoTerminal s3 = new NoTerminal("s3", 0, 0);
+                NoTerminal s3 = new NoTerminal("s3", 0, 0, false);
                 suma(i1, s2.getValor(), s3);
                 procEL(s3.getValor(), s1);
                 return;
 
             case '-':
                 avance();
-                NoTerminal s4 = new NoTerminal("s4", 0, 0);
+                NoTerminal s4 = new NoTerminal("s4", 0, 0, false);
                 procT(s4);
-                NoTerminal s5 = new NoTerminal("s5", 0, 0);
+                NoTerminal s5 = new NoTerminal("s5", 0, 0, false);
                 resta(i1, s4.getValor(), s5);
                 procEL(s5.getValor(), s1);
                 return;
@@ -109,7 +265,7 @@ public class ApCompiladorRecursivoAtributoEL {
         switch (sim) {
             case 'i':
             case '(':
-                NoTerminal s2 = new NoTerminal("s2", 0, 0);
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false);
 
                 procP(s2);
                 System.out.println("Valor de <P> " + s2.getValor());
@@ -125,16 +281,16 @@ public class ApCompiladorRecursivoAtributoEL {
 
     public static void procTL(double i1, NoTerminal s1) {
         // <T_L> --> *<P><T_L>
-        // <T_L> --> /<P><T_L>   no está implementada
+        // <T_L> --> /<P><T_L>
         // <T_L> --> e
 
         switch (sim) {
             case '*':
 
                 avance();
-                NoTerminal s2 = new NoTerminal("s2", 0, 0);
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false);
                 procP(s2);
-                NoTerminal s3 = new NoTerminal("s3", 0, 0);
+                NoTerminal s3 = new NoTerminal("s3", 0, 0, false);
                 mul(i1, s2.getValor(), s3);
                 procTL(s3.getValor(), s1);
                 return;
@@ -142,9 +298,9 @@ public class ApCompiladorRecursivoAtributoEL {
             case '/':
 
                 avance();
-                NoTerminal s4 = new NoTerminal("s4", 0, 0);
+                NoTerminal s4 = new NoTerminal("s4", 0, 0, false);
                 procP(s4);
-                NoTerminal s5 = new NoTerminal("s5", 0, 0);
+                NoTerminal s5 = new NoTerminal("s5", 0, 0, false);
                 div(i1, s4.getValor(), s5);
                 procTL(s5.getValor(), s1);
                 return;
@@ -165,12 +321,12 @@ public class ApCompiladorRecursivoAtributoEL {
     public static void procP(NoTerminal s1) {
 
         // <P> --> <F><P-L>
-        //SOLO DEBE QUEDAR <P> --> <F><P-L>
-//ESTA ES LA TRANSICION CORRECTA PARA QUE P --> <F><PL>
+        // SOLO DEBE QUEDAR <P> --> <F><P-L>
+        // ESTA ES LA TRANSICION CORRECTA PARA QUE P --> <F><PL>
         switch (sim) {
             case 'i':
             case '(':
-                NoTerminal s2 = new NoTerminal("s2", 0, 0);
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false);
 
                 procF(s2);
                 System.out.println("Valor de <F> " + s2.getValor());
@@ -186,15 +342,15 @@ public class ApCompiladorRecursivoAtributoEL {
     }
 
     public static void procPL(double i1, NoTerminal s1) {
-// <P-L> --> ^<F><P-L>
-// <P-L> --> e
+        // <P-L> --> ^<F><P-L>
+        // <P-L> --> e
 
         switch (sim) {
             case '^':
                 avance();
-                NoTerminal s2 = new NoTerminal("s2", 0, 0);
+                NoTerminal s2 = new NoTerminal("s2", 0, 0, false);
                 procF(s2);
-                NoTerminal s3 = new NoTerminal("s3", 0, 0);
+                NoTerminal s3 = new NoTerminal("s3", 0, 0, false);
                 pot(i1, s2.getValor(), s3);
                 procPL(s3.getValor(), s1);
                 return;
@@ -215,12 +371,12 @@ public class ApCompiladorRecursivoAtributoEL {
     }
 
     public static void procF(NoTerminal s1) {
-        //ESTOS ERAN LOS CASOS Q TENIA P
+        // ESTOS ERAN LOS CASOS Q TENIA P
         // <P> --> i
         // <P> --> ( <E> )
-        //LOS CASOS DE ARRIBA TIENE QUE TENERLOS F
-//        F --> (<ELO>)
-//        F --> I    
+        // LOS CASOS DE ARRIBA TIENE QUE TENERLOS F
+        // F --> (<ELO>)
+        // F --> I
 
         switch (sim) {
             case 'i':
@@ -258,12 +414,14 @@ public class ApCompiladorRecursivoAtributoEL {
     }
 
     public static void mul(double i1, double i2, NoTerminal s3) {
+        System.out.println("Elementos a multiplicar" + i1 + " " + i2);
         s3.setValor(i1 * i2);
     }
 
     public static void div(double i1, double i2, NoTerminal s3) {
+        System.out.println("Elementos a dividir" + 1 + " " + i2);
         if (i2 == 0) {
-            System.out.println("Erro Division por cero");
+            System.out.println("Error Division por cero");
             rechace();
         } else {
             s3.setValor(i1 / i2);
@@ -272,6 +430,7 @@ public class ApCompiladorRecursivoAtributoEL {
     }
 
     public static void pot(double i1, double i2, NoTerminal s3) {
+        System.out.println("Elementos para potencia" + i1 + " " + i2);
         s3.setValor(Math.pow(i1, i2));
     }
 
@@ -280,8 +439,10 @@ public class ApCompiladorRecursivoAtributoEL {
     }
 
     public static void analisisLexico() {
-        // Este analizador es sencillo determina solo constantes enteras y reales positivas
-        // Trabaja los diferentes elementos en un ArrayList que trabaja con la clase Clexico
+        // Este analizador es sencillo determina solo constantes enteras y reales
+        // positivas
+        // Trabaja los diferentes elementos en un ArrayList que trabaja con la clase
+        // Clexico
         // la cual define el ArrayList con la clase CElemento
         // Almacen los valores para poder hallar los resultados
 
@@ -329,7 +490,7 @@ public class ApCompiladorRecursivoAtributoEL {
             lex1.adicionarElemento(ele1);
 
             ind = ind + 1;
-            //System.out.print("indice ="+ind);
+            // System.out.print("indice ="+ind);
 
         } // después del while se adiciona el fin de secuencia al léxico
         ele1 = new Elemento('¬', 0, ind);
--- src/apcompiladorrecursivoatributoel/NoTerminal.java
+++ src/apcompiladorrecursivoatributoel/NoTerminal.java
@@ -1,55 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package apcompiladorrecursivoatributoel;
 
-/**
- *
- * @author SistemaInvestigacion
- */
 public class NoTerminal {
- 
-    //Atributos
-    
+
+    // Atributos
     private String nombre;
     private int direc;
     private double valor;
-    
-    
-    // Métodos
-    
-    public NoTerminal(){
-     
+    private boolean vlogico;
+
+    public NoTerminal(String nombre, int direc, double valor, boolean vlogico) {
+        this.nombre = nombre;
+        this.direc = direc;
+        this.valor = valor;
+        this.vlogico = vlogico;
     }
-    public NoTerminal(String nom,int val1,double val2){
-        nombre=nom;
-        direc=val1;
-        valor=val2;
-        
-    }
-    public String getNombre(){
+
+    public String getNombre() {
         return nombre;
     }
-    public int getDirec(){
-        return (int)direc;
-    }
-    public double getValor(){
-        return (double)valor;
-    }
-    
-    public void setNombre(String nom){
 
-        nombre=nom;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    
-    public void setDirec(int val1){
-        direc=val1;
+
+    public int getDirec() {
+        return direc;
     }
-    public void setValor(double val2){
-        valor=val2;
+
+    public void setDirec(int direc) {
+        this.direc = direc;
     }
-    
-    
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean isVlogico() {
+        return vlogico;
+    }
+
+    public void setVlogico(boolean vlogico) {
+        this.vlogico = vlogico;
+    }
+
 }
